0d5c21f1fe539086c80efb72cf1162d0c2291fba
bbbirkan <58374229+bbbirkan@users.noreply.github.com>
2022-03-02 22:42:13 -0500

    started

---
+++ main.py
@@ -0,0 +1,35 @@
+import numpy as np
+import datetime as dt
+import pandas_datareader as pdr
+
+def getData(stocks, start, end):
+    stockData = pdr.get_data_yahoo(stocks, start=start, end=end)
+    stockData = stockData['Close']
+
+    returns = stockData.pct_change()
+    meanReturns = returns.mean()
+    covMatrix = returns.cov()
+    return meanReturns, covMatrix
+
+
+def portfolioPerformance(weights, meanReturns, covMatrix):
+    returns = np.sum(meanReturns*weights)*252
+    std = np.sqrt(
+            np.dot(weights.T,np.dot(covMatrix, weights))
+           )*np.sqrt(252)
+    return returns, std
+
+stocklist = ['AAPL', 'MSFT', 'GOOG', 'AMZN']
+# stocks=[stock for stock in stocklist]
+# print(stocks)
+
+weights = np.array([0.2, 0.3, 0.1, 0.4])
+
+endDate = dt.datetime.now()
+startDate = endDate - dt.timedelta(days=365)
+print(getData(stocklist, startDate, endDate))
+
+meanReturns, covMatrix=getData(stocklist, startDate, endDate)
+returns, std = portfolioPerformance(weights, meanReturns, covMatrix)
+
+print(round(returns*100,2), round(std*100,2))
